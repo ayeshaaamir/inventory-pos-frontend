7b7fede223615ef580294f80fc8690f7411d8789
ayeshaaamir <ayeshaaamir38@gmail.com>
2024-11-27 00:54:20 +0000

    ip updated..

--- inventory-pos-frontend/src/config/constants.ts
+++ inventory-pos-frontend/src/config/constants.ts
@@ -1,2 +1,3 @@
 // export const API_BASE_URL = "http://localhost:5000/api/";
-export const API_BASE_URL = "http://172.20.10.2:5000/api/";
+// export const API_BASE_URL = "http://172.20.10.2:5000/api/";
+export const API_BASE_URL = "http://192.168.0.15:5000/api/";
