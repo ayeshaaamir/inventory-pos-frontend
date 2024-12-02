adc759509b01de4770f2d94dd4be2e870e23999c
ayeshaaamir <ayeshaaamir38@gmail.com>
2024-12-02 23:58:20 +0000

    prime react added..

--- inventory-pos-frontend/src/App.jsx
+++ inventory-pos-frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import { Button } from 'primereact/button';   
 
 function App() {
-  return <h1 className="text-2xl font-bold underline">Hello world!</h1>;
+  return <Button label="Show" />;
 }
 
 export default App;
--- inventory-pos-frontend/src/main.jsx
+++ inventory-pos-frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import PrimeReact from "primereact/api";
 
-createRoot(document.getElementById('root')).render(
+PrimeReact.ripple = true;
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
